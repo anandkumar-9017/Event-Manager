0e8e617bb6ae01c1e7e1e59f2f5960a28150ba2d
LENOVO <anandkum9182@gmail.com>
2020-10-08 22:58:37 +0530

    added home page

---
+++ js/js_main.js
@@ -0,0 +1,20 @@
+﻿/*
+    TABLE OF CONTENTS
+    ---------------------------
+     1. Loading
+*/
+
+/* ================================= */
+/* :::::::::: 1. Loading ::::::::::: */
+/* ================================= */
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    $('.videobg').css({
+        "opacity": "0.0"
+    });
+}
+$(window).load(function () {
+
+    $(".loader-icon").delay(1000).fadeOut();
+    $(".page-loader").delay(1500).fadeOut("slow");
+
+});
